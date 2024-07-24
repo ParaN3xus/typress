8fa7547739bf472c73bd7b93a9c18b818590e26c
ParaN3xus <paran3xus007@gmail.com>
2024-07-24 12:19:56 +0800

    fix: convert rgb

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, request, jsonify
 import os
 from PIL import Image
 from frontend import index_html
-from typress.model.model import load_model
+from typress.model.model import load_model, generate
 from dotenv import load_dotenv
+import torch
 
 load_dotenv()
 
@@ -12,29 +13,32 @@ load_dotenv()
 app = Flask("Typress OCR")
 
 # Load model and processor
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model, processor = load_model(os.getenv("MODEL_PATH"))
+model.to(device)
 
 
-@app.route('/api/formula', methods=['POST'])
+@app.route("/api/formula", methods=["POST"])
 def get_formula():
-    if 'image' not in request.files:
+    if "image" not in request.files:
         return jsonify({"error": "No image file provided"}), 400
 
-    image = request.files['image']
-    img = Image.open(BytesIO(image.read()))
+    image = request.files["image"]
+    img = Image.open(image).convert("RGB")
     try:
-        pixel_values = processor(
-            images=[img], return_tensors="pt").pixel_values
-        generated_text = generate_formula(model, processor, pixel_values)
+        pixel_values = processor(images=img, return_tensors="pt").pixel_values.to(
+            device
+        )
+        generated_text = generate(model, processor, pixel_values)[0]
         return jsonify({"formula": generated_text})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     return index_html()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
