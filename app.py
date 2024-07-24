c6407507a2b9458f0f874e45fe6b81eafa0a2c4b
sjfhsjfh <sjfhsjfh@qq.com>
2024-07-24 11:30:49 +0800

    feat: add basic frontend

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from io import BytesIO
 from flask import Flask, request, jsonify
 import os
+from PIL import Image
+from frontend import index_html
 from typress.model.model import load_model
 from dotenv import load_dotenv
 
@@ -18,13 +21,19 @@ def get_formula():
         return jsonify({"error": "No image file provided"}), 400
 
     image = request.files['image']
+    img = Image.open(BytesIO(image.read()))
     try:
         pixel_values = processor(
-            images=[image], return_tensors="pt").pixel_values
+            images=[img], return_tensors="pt").pixel_values
         generated_text = generate_formula(model, processor, pixel_values)
         return jsonify({"formula": generated_text})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return index_html()
 
 
 if __name__ == '__main__':
